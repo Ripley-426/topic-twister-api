270ce1ab7219a0851dc0113047865d5a2ba0fa9e
eliosinhache <elioh93@gmail.com>
2022-02-09 23:16:22 -0300

    Tdd of match, round and roundShould

--- src/main/kotlin/model/Round.kt
+++ src/main/kotlin/model/Round.kt
@@ -1,7 +1,38 @@
 package com.example.model
 
-class Round {
+import com.example.services.TopicRandomizer
+import com.example.services.WordValidator
 
+class Round (
+    val letter: String
+        ) {
 
+    var topics: List<String> = arrayListOf()
+    val topicRandomizer = TopicRandomizer()
+    val wordsValidator = WordValidator()
+    val wordsValidations = mutableListOf<Boolean>()
+    var words: MutableList<String> = mutableListOf()
 
+    init {
+        topics = topicRandomizer.GetRandomTopics(5)
+    }
+
+    fun readTopics(): List<String> {
+        return topics
+    }
+
+    fun addWords(wordsList: MutableList<String>) {
+        words = wordsList
+        validateWords()
+    }
+
+    fun validateWords() {
+        convertToTopicsAndWords()
+
+        wordsValidations = wordsValidator.Validate()
+    }
+
+    private fun convertToTopicsAndWords() {
+
+    }
 }
--- src/main/kotlin/model/RoundShould.kt
+++ src/main/kotlin/model/RoundShould.kt
@@ -1,13 +1,48 @@
 package model
 
+import com.example.model.Round
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class RoundShould {
 
-    @Test
-    fun `Sample test`() {
+    private lateinit var letter: String
+    private lateinit var round: Round
 
+    @BeforeEach
+    fun setup(){
+        letter = "A"
+        round = Round(letter)
     }
+
+    @Test
+    fun `Have A Letter`() {
+
+        //When
+        val result = round.letter
+
+        //Then
+        assertEquals(letter, result)
+    }
+
+    @Test
+    fun `Have Topics`() {
+
+        assertEquals(5,round.readTopics().count())
+    }
+
+    @Test
+    fun `Validate Words After Adding them`() {
+
+        val expectedBoolList: MutableList<Boolean> = mutableListOf(true, false, true, false, true)
+        round.addWords(mutableListOf("A", "B", "A", "B", "A"))
+
+
+        val result = round.getPlayerAWordsValidation()
+
+        assertEquals(expectedBoolList, result)
+    }
+
 
 }
