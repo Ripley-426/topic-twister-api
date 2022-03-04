2e2fd97d29813f9c6bc8fd3ffc5951b6f1afc3eb
Ripley-426 <nkotolenko@gmail.com>
2022-03-04 16:59:08 -0300

    Revert "Update Round.kt"
    This reverts commit d29df658d803f6dc197e2498a189eb95a1b4c7b4.

--- src/main/kotlin/model/Round.kt
+++ src/main/kotlin/model/Round.kt
@@ -41,30 +41,11 @@ open class Round (val roundNumber: Int,
 
     fun addWords(wordsList: MutableList<String>) {
 
-        var curatedWordList: MutableList<String> = mutableListOf()
-        curatedWordList = if (wordsList.isEmpty() or (wordsList.size < 5)) {
-            FillWordList(wordsList)
-        } else {
-            wordsList
-        }
-
-        setWordsToPlayersAndValidate(curatedWordList)
+        setWordsToPlayersAndValidate(wordsList)
 
         changeTurn()
 
         if (turn == Turn.FINISHED) { calculateRoundScore() }
-    }
-
-    fun FillWordList(wordList: MutableList<String>): MutableList<String> {
-        var newList = mutableListOf<String>()
-        for (i in 1..wordList.size) {
-            newList.add(wordList[i])
-        }
-        for (i in wordList.size+1..5) {
-            newList.add("")
-        }
-
-        return newList
     }
 
     fun getTurnInt():Int {
