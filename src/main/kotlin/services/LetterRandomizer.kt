7c197623397188a5232c4b884f7400175030125a
Mariano Wiñar <50705701+winarmarianoj@users.noreply.github.com>
2022-03-04 23:06:48 -0300

    Fixed duplicated letter

--- src/main/kotlin/services/LetterRandomizer.kt
+++ src/main/kotlin/services/LetterRandomizer.kt
@@ -4,9 +4,15 @@ import com.example.interfaces.ILetterRandomizer
 
 class LetterRandomizer: ILetterRandomizer {
 
+    var letterList : MutableList<Char>  = mutableListOf()
+
     override fun getRandomLetter(): Char {
+        var letter = ('A'..'Z').random()
+        while (letterList.contains(letter)){
+            letter = ('A'..'Z').random()
+        }
+        letterList.add(letter)
 
-        return ('A'..'Z').random()
-
+        return letter
     }
 }
